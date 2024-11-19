8041c9735b836218626dc724a103ab3261c0e865
Frederick Molesworth-St aubyn <molesworth-staubynf@cardiff.ac.uk>
2024-11-19 13:38:30 +0000

    attempted addition of static/resources

---
+++ clientProject/src/main/java/com/team14/clientProject/profilePage/ProfilePage.java
@@ -0,0 +1,8 @@
+package com.team14.clientProject.profilePage;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+@Controller
+public class ProfilePage {
+
+
+}
